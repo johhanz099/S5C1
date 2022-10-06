5a860b691a342fe6731192bd50ee6ca14781a509
johhanz099 <banaya@unal.edu.co>
2022-10-06 15:59:15 -0500

    Working on OndaFunct

--- S8/AnayaBryan_S8C1EDP.cpp
+++ S8/AnayaBryan_S8C1EDP.cpp
@@ -9,14 +9,14 @@ typedef vector<double> vec; //nombre para vector
 /*
 Inicializar método ecuación de onda
 */
-void eqOnda(float c, float L, float h, float N);
+void eqOnda(float c, float L, float h, float N, vec & xInic, vec & yInic);
 
 
 /*
 Inicializar método Función de condición inicial
 Crea el arreglo (vector) con los valores que representan la condición inicial
 */
-void FcondInicial(float L, float h, float N);
+void FcondInicial(float L, float h, float N, vec & xInic, vec & yInic);
 
 
 /*
@@ -27,9 +27,12 @@ int main(void){
     float h = 0.1; // altura inicial de la cuerda
 	float L = 2; // longitud de la cuerda
     float N = 100; // Cantidad de divisiones discretas - N par
+    float tf= 0.1; // tiempo final
     
     // Prueba implementar funcion condición inicial
-    FcondInicial(L,h,N); //Funcionó correctamente
+    vec xInic(N,0);
+    vec yInic(N,0);
+    FcondInicial(L,h,N,xInic,yInic); //Funcionó correctamente
     
     //vec condInicialx = FcondInicial(L,h,N)[0];
     //vec condInicialy = FcondInicial(L,h,N)[1];
@@ -40,23 +43,25 @@ int main(void){
 /*
 Método ecuación de onda
 */
-void eqOnda(float c, float L, float h, float N){
-    // Crear la condición inicial
-    float m = 2*h/L; // pendiente
-    float dx = L/N; // avance discreto en x
-    vec x_cond(N,0); // Inicializar vector en x
-    vec y_cond(N,0); // --------------------- y
-    float b = (2*h + m*L)/2; // punto de corte y_2
+void eqOnda(float c, float L, float h, float N, vec & xInic, vec & yInic, float tf){
+    float dx = L/N;
+    float dt = 0.5*dx/c;
+    float k  = c*c*dt*dt*dx*dx;
+    vec t(N,0);
+    vec upast = yInic;
+    int ntmax = tf/dt;
     
-    for(int i = 0; i <= N; i++){
-        x_cond[i] = dx * i; // avance en x
-        if(x_cond[i] <= L/2){ // arreglo y_1
-            y_cond[i] = x_cond[i] * m;
-        }
-        else if(x_cond[i] > L/2){ // arreglo y_2
-            y_cond[i] = x_cond[i] * (-m) + b;
-        }
+    //Open data
+    
+    for (int j = 0; j <= ntmax; j++){
+        t[j] = dt * j; // avance en t
+        
+        for (int i = 0, i <= N, i++){
+            u[j] = 
+            
     }
+    
+    
     
     
 }
@@ -65,7 +70,7 @@ void eqOnda(float c, float L, float h, float N){
 /*
 Método Función de condición inicial
 */
-void FcondInicial(float L, float h, float N){
+void FcondInicial(float L, float h, float N, vec & xInic, vec & yInic){
     float m = 2*h/L; // pendiente
     float dx = L/N; // avance discreto en x
     vec x(N,0); // Inicializar vector en x
@@ -89,7 +94,8 @@ void FcondInicial(float L, float h, float N){
     }
     // Close data
     outfile.close();
-    //return x,y;
+    xInic = x;
+    yInic = y;
 }
     
     
