86589cd0110864d467b9eb493cfde2ba3b0509b3
johhanz099 <banaya@unal.edu.co>
2022-10-18 19:43:06 +0000

    FaltaGraficar

---
+++ S9/PLOTS_AnayaBryan_S9C2_EDP.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+data = np.genfromtxt("data2d_Difusion.dat", delimiter=",")
+datap= np.genfromtxt("prub.dat", delimiter=",")
+
+def separateData(data,filas,cols):
+    dataSep = np.zeros((filas,cols))
+    n = 0
+    ##while n <= cols:
+        
+                
+            
+    return 0
+
+print(data)
+#print("filas:",datap.shape[0],"Cols:",datap.shape[1])
