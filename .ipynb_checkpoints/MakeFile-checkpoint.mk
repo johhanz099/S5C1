a5f7793307a3feaadf0ba8a5e37c2fc7274c904c
johhanz099 <banaya@unal.edu.co>
2022-10-31 16:39:04 +0000

    Archivos Actualizados

--- .ipynb_checkpoints/MakeFile-checkpoint.mk
+++
@@ -1,9 +0,0 @@
-all: fig1_dataRand.png fig2_dataImp.png
-
-fig1_dataRand.png: AnayaBryan_S5C2_repasoC.cpp AnayaBryan_S6C1_Graf.py arrAleat.dat
-    g++ AnayaBryan_S5C2_repasoC.cpp
-    python AnayaBryan_S6C1_Graf.py
-    
-fig2_dataImp.png: AnayaBryan_S5C2_repasoC.cpp AnayaBryan_S6C1_Graf.py arrImp.dat
-    g++ AnayaBryan_S5C2_repasoC.cpp
-    python AnayaBryan_S6C1_Graf.py    
